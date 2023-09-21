cff0bf50f7441b83a3959d1012b53ab6c5f4289a
Md.Raysu kobir <86109661+raysulkobir@users.noreply.github.com>
2023-09-21 14:49:40 +0600

    add singleton pattern done

--- Creational/Singleton Pattern/index.php
+++ Creational/Singleton Pattern/index.php
@@ -1,12 +1,12 @@
-<?php include "inc/header.php" ?>
+ 
 
-Singleton pattern is one of the simplest design patterns in php. This type of design pattern comes under creational pattern as this pattern provides one of the best ways to create an object.
-<br/>
-This pattern involves a single class which is responsible to create an object while making sure that only single object gets created. This class provides a way to access its only object which can be accessed directly without need to instantiate the object of the class.
-<hr />
-    <h1>Singleton</h1>
-<hr />
     <?php
+    //     Singleton pattern is one of the simplest design patterns in php. This type of design pattern comes under creational pattern as this pattern provides one of the best ways to create an object.
+    // <br/>
+    // This pattern involves a single class which is responsible to create an object while making sure that only single object gets created. This class provides a way to access its only object which can be accessed directly without need to instantiate the object of the class.
+    // <hr />
+    //     <h1>Singleton</h1>
+    // <hr />
 
 
     class Singleton
@@ -43,4 +43,4 @@ This pattern involves a single class which is responsible to create an object wh
 
     ?>
 
-    <?php include "inc/footer.php" ?>
+ 
